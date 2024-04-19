d78f4041413b63cf994072f426c3b427593b2064
Cristhiam D. López Nuñez <cristhiam.lopez.nunez@gmail.com>
2024-04-19 16:52:18 -0400

    Added more ui elements and details

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,20 +1,47 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, HostListener, Renderer2, ViewChild } from '@angular/core';
 import { ProductCardComponent } from "../product-card/product-card.component";
 
 @Component({
-    selector: 'app-product-list',
-    standalone: true,
-    templateUrl: './product-list.component.html',
-    styleUrl: './product-list.component.css',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [
-        CommonModule,
-        ProductCardComponent
-    ]
+  selector: 'app-product-list',
+  standalone: true,
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    CommonModule,
+    ProductCardComponent
+  ]
 })
 export class ProductListComponent {
 
-  public items = [0,0,0,0,0,0,0,0,0,0];
+  public scrollClass: string = 'top-0';
+  public position: number = 0;
+  public items = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  @ViewChild('navbar') navbar!: ElementRef<HTMLInputElement>;
+
+  constructor() { }
+
+
+  @HostListener('document:scroll', ['$event'])
+  public onViewportScroll() {
+
+    const scrollAmount = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollAmount > this.position) {
+      this.scrollClass = '-top-16';
+    } else {
+      this.scrollClass = 'top-0';
+    }
+
+    this.position = scrollAmount
+
+
+
+
+
+  }
+
 
 }
