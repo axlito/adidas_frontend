ffbaf1535403ab2e9efe9969d48badcdf8178369
Cristhiam D. López Nuñez <cristhiam.lopez.nunez@gmail.com>
2024-04-19 18:58:26 -0400

    Added basic API integration for testing

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-product-card',
@@ -11,4 +12,35 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './product-card.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductCardComponent { }
+export class ProductCardComponent {
+
+  @Input({ required: true }) product!: Product;
+
+  handleReviews(product: Product) {
+
+    let stars = [0, 0, 0, 0, 0];
+
+    if (product.Reviews) {
+      const value = Math.round(product.Reviews.rating);
+      switch (value) {
+        case 1:
+          stars = [1, 0, 0, 0, 0];
+          break;
+        case 2:
+          stars = [1, 1, 0, 0, 0];
+          break;
+        case 3:
+          stars = [1, 1, 1, 0, 0];
+          break;
+        case 4:
+          stars = [1, 1, 1, 1, 0];
+          break;
+        case 5:
+          stars = [1, 1, 1, 1, 1];
+          break;
+      }
+    }
+    return stars
+  }
+
+}
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, ElementRef, HostListener, Renderer2, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild, computed, signal } from '@angular/core';
 import { ProductCardComponent } from "../product-card/product-card.component";
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-product-list',
@@ -13,16 +15,30 @@ import { ProductCardComponent } from "../product-card/product-card.component";
     ProductCardComponent
   ]
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
 
   public scrollClass: string = 'top-0';
   public position: number = 0;
-  public items = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  private _productsList = signal<Product[] | null>(null);
+  public productsList = computed(() => this._productsList());
+
 
   @ViewChild('navbar') navbar!: ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
+  ngOnInit(): void {
+    this.getProductsList();
+    console.log(this.productsList());
+
+  }
+
+  public getProductsList(): void {
+    this.productService.getGamesList()
+      .subscribe((result: Product[]) => {
+        this._productsList.set(result);
+      });
+  }
 
   @HostListener('document:scroll', ['$event'])
   public onViewportScroll() {
